8596da17d369f2f2f449992fb46e48ca45b68ce2
N3cr0 <luka_jovanovic@Yahoo.com>
2020-03-03 23:42:52 +0100

    Add files via upload

--- clinet.py
+++ clinet.py
@@ -1,15 +1,28 @@
 import socket
 import pyautogui
 
-host = str(input("Input Server name"))
-port = int(input("Input port number"))
+host = str(input("Input Server IP address: "))
+port = int(input("Input port number: "))
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, 1234))
+while True:
+	try:
+		s.connect((host, 1234))
+		break
+	except:
+		continue
 msg = s.recv(1024).decode()
 print(msg)
 while True:
 	msg = s.recv(1024).decode()
-	if msg.find("press") :
-                pyautogui.keyDown(msg[-2])
-	if msg.find("release") :
-		pyautogui.keyUp(msg[-2])
+	if (msg.find("Key")>0):
+		st = msg.find('Key') +4
+		if msg.find("release"):
+			pyautogui.keyDown(msg[st:])
+		if msg.find("press"):
+			pyautogui.keyUp(msg[st:])
+	else:
+		st = msg.find('\'') +1
+		if msg.find("release"):
+			pyautogui.keyDown(msg[st:-1])
+		if msg.find("press"):
+			pyautogui.keyUp(msg[st:-1])
